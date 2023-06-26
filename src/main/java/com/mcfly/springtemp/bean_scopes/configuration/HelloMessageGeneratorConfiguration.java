db6f0532df81284f641551325b21c163ef14daeb
Andrey Polnikov <polnikov.andrey@gmail.com>
2023-06-26 17:47:00 +0300

    + bean web scopes

--- src/main/java/com/mcfly/springtemp/bean_scopes/configuration/HelloMessageGeneratorConfiguration.java
+++ src/main/java/com/mcfly/springtemp/bean_scopes/configuration/HelloMessageGeneratorConfiguration.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
 
 @Configuration
 public class HelloMessageGeneratorConfiguration {
@@ -23,8 +26,31 @@ public class HelloMessageGeneratorConfiguration {
     }
 
     @Bean
-    @Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+    @RequestScope
     public HelloMessageGenerator requestScopeHelloMessageGenerator() {
+        return new HelloMessageGenerator();
+    }
+
+    @Bean
+    @SessionScope
+    public HelloMessageGenerator sessionScopeHelloMessageGenerator() {
+        return new HelloMessageGenerator();
+    }
+
+    @Bean
+    @ApplicationScope
+    public HelloMessageGenerator applicationScopeHelloMessageGenerator() {
+        return new HelloMessageGenerator();
+    }
+
+    /*
+      When first accessed, WebSocket scoped beans are stored in the WebSocket session attributes.
+      The same instance of the bean is then returned whenever that bean is accessed during the entire WebSocket session.
+      We can also say that it exhibits singleton behavior, but limited to a WebSocket session only.
+      */
+    @Bean
+    @Scope(value = "websocket", proxyMode = ScopedProxyMode.TARGET_CLASS)
+    public HelloMessageGenerator websocketScopeHelloMessageGenerator() {
         return new HelloMessageGenerator();
     }
 
--- src/main/java/com/mcfly/springtemp/bean_scopes/controller/HelloMessageController.java
+++ src/main/java/com/mcfly/springtemp/bean_scopes/controller/HelloMessageController.java
@@ -3,11 +3,42 @@ package com.mcfly.springtemp.bean_scopes.controller;
 import com.mcfly.springtemp.bean_scopes.common.HelloMessageGenerator;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloMessageController {
 
     @Resource(name = "requestScopeHelloMessageGenerator")
-    HelloMessageGenerator helloMessageGenerator;
+    HelloMessageGenerator requestScopeHelloMessageGenerator;
 
+    @Resource(name = "sessionScopeHelloMessageGenerator")
+    HelloMessageGenerator sessionScopeHelloMessageGenerator;
+
+    @Resource(name = "applicationScopeHelloMessageGenerator")
+    HelloMessageGenerator applicationScopeHelloMessageGenerator;
+
+    @RequestMapping("/scopes/request")
+    public String getRequestScopeMessage(Model model) {
+        model.addAttribute("previousMessage", requestScopeHelloMessageGenerator.getMessage());
+        requestScopeHelloMessageGenerator.setMessage("Good morning!");
+        model.addAttribute("currentMessage", requestScopeHelloMessageGenerator.getMessage());
+        return "bean_scopes/scopesExample";
+    }
+
+    @RequestMapping("/scopes/session")
+    public String getSessionScopeMessage(Model model) {
+        model.addAttribute("previousMessage", sessionScopeHelloMessageGenerator.getMessage());
+        sessionScopeHelloMessageGenerator.setMessage("Good afternoon!");
+        model.addAttribute("currentMessage", sessionScopeHelloMessageGenerator.getMessage());
+        return "bean_scopes/scopesExample";
+    }
+
+    @RequestMapping("/scopes/application")
+    public String getApplicationScopeMessage(Model model) {
+        model.addAttribute("previousMessage", applicationScopeHelloMessageGenerator.getMessage());
+        applicationScopeHelloMessageGenerator.setMessage("Good evening!");
+        model.addAttribute("currentMessage", applicationScopeHelloMessageGenerator.getMessage());
+        return "bean_scopes/scopesExample";
+    }
 }
