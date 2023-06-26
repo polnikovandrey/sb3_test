255eed5f6f4178e5f92bca9799f5ede8d4e26e65
Andrey Polnikov <polnikov.andrey@gmail.com>
2023-06-26 14:34:18 +0300

    + PersonRepositoryLayerUnitTest.testGetNotValidPerson + PersonRepositoryLayerUnitTest.testCreatePerson + PersonRepositoryLayerUnitTest.testDeletePerson

--- src/test/java/com/mcfly/springtemp/PersonRepositoryLayerUnitTest.java
+++ src/test/java/com/mcfly/springtemp/PersonRepositoryLayerUnitTest.java
@@ -13,8 +13,10 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest        // test environment for JPA layer
@@ -44,5 +46,34 @@ public class PersonRepositoryLayerUnitTest {
         assertThat(persons.get(0).getName()).isEqualTo("A");
         assertThat(persons.get(1).getId()).isNotNegative().isGreaterThan(0);
         assertThat(persons.get(1).getName()).isEqualTo("B");
+    }
+
+    @Test
+    public void testGetNotValidPerson() {
+        final Exception exception = assertThrows(NoSuchElementException.class, () -> personRepository.findById(100L).get());
+        assertThat(exception).isNotNull();
+        assertThat(exception.getClass()).isEqualTo(NoSuchElementException.class);
+        assertThat(exception.getMessage()).isEqualTo("No value present");
+    }
+
+    @Test
+    public void testCreatePerson() {
+        final Person personC = new Person(null, "C");
+        final Person savedPerson = personRepository.save(personC);
+        assertThat(savedPerson).isNotNull();
+        assertThat(savedPerson.getId()).isNotNull().isGreaterThan(1);
+        assertThat(savedPerson.getName()).isNotNull();
+        assertThat(savedPerson).usingRecursiveComparison().ignoringFields("id").isEqualTo(personC);
+    }
+
+    @Test
+    public void testDeletePerson() {
+        final Person personC = new Person(null, "C");
+        final Person savedPerson = personRepository.save(personC);
+        personRepository.delete(savedPerson);
+        final Exception exception = assertThrows(NoSuchElementException.class, () -> personRepository.findById(savedPerson.getId()).get());
+        assertThat(exception).isNotNull();
+        assertThat(exception.getClass()).isEqualTo(NoSuchElementException.class);
+        assertThat(exception.getMessage()).isEqualTo("No value present");
     }
 }
