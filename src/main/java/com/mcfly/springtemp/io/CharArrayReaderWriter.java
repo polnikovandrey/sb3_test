4465daaf91495a61a656701b9b43f7227ab24201
Andrey Polnikov <polnikov.andrey@gmail.com>
2023-08-07 01:03:59 +0300

    + StringReaderWriter

--- src/main/java/com/mcfly/springtemp/io/CharArrayReaderWriter.java
+++ src/main/java/com/mcfly/springtemp/io/CharArrayReaderWriter.java
@@ -2,9 +2,7 @@ package com.mcfly.springtemp.io;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.CharArrayReader;
-import java.io.CharArrayWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Arrays;
 
 @Slf4j
@@ -16,17 +14,20 @@ public class CharArrayReaderWriter {
 
     private static void writeBytesToFile() {
         char[] chars = new char[10];
-        try (CharArrayWriter caw = new CharArrayWriter(10)) {
-            caw.write(new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' });
+        try (CharArrayWriter caw = new CharArrayWriter(10);
+             BufferedWriter bw = new BufferedWriter(caw)) {
+            bw.write(new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' });
+            bw.flush();
             chars = caw.toCharArray();
             log.info("Successfully wrote char array to CharArrayWriter: {}", Arrays.toString(chars));
         } catch (IOException e) {
             log.error("Exception: ", e);
         }
-        try (CharArrayReader car = new CharArrayReader(chars)) {
+        try (CharArrayReader car = new CharArrayReader(chars);
+             BufferedReader br = new BufferedReader(car)) {
             final StringBuilder sb = new StringBuilder();
             int aChar;
-            while ((aChar = car.read()) > -1) {
+            while ((aChar = br.read()) > -1) {
                 sb.append((char)aChar);
             }
             log.info("Successfully read char array from CharArrayReader: {}", Arrays.toString(sb.toString().toCharArray()));
