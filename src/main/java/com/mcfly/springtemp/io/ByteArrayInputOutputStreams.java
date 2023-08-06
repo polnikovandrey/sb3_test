a3405b2b8557bbfc21ccf70cef59d865e32c2d30
Andrey Polnikov <polnikov.andrey@gmail.com>
2023-08-07 01:05:28 +0300

    + StringReaderWriter

--- src/main/java/com/mcfly/springtemp/io/ByteArrayInputOutputStreams.java
+++ src/main/java/com/mcfly/springtemp/io/ByteArrayInputOutputStreams.java
@@ -2,9 +2,7 @@ package com.mcfly.springtemp.io;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.Arrays;
 
 @Slf4j
@@ -16,15 +14,18 @@ public class ByteArrayInputOutputStreams {
 
     private static void writeAndReadBytes() {
         byte[] bytes = new byte[10];
-        try (ByteArrayOutputStream baos = new ByteArrayOutputStream(10)) {
-            baos.writeBytes(new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream(10);
+             BufferedOutputStream bos = new BufferedOutputStream(baos)) {
+            bos.write(new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+            bos.flush();
             bytes = baos.toByteArray();
             log.info("Successfully wrote byte array to ByteArrayOutputStream: {}", Arrays.toString(bytes));
         } catch (IOException e) {
             log.error("Exception: ", e);
         }
-        try (ByteArrayInputStream bais = new ByteArrayInputStream(bytes)) {
-            final byte[] bytesRead = bais.readAllBytes();
+        try (ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+             BufferedInputStream bis = new BufferedInputStream(bais)) {
+            final byte[] bytesRead = bis.readAllBytes();
             log.info("Successfully read byte array from ByteArrayInputStream: {}", Arrays.toString(bytesRead));
         } catch (IOException e) {
             log.error("Exception: ", e);
