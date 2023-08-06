c9d562788add0d69b4918861330db5f6bb22b400
Andrey Polnikov <polnikov.andrey@gmail.com>
2023-08-07 00:17:43 +0300

    + BAIS / BAOS

--- src/main/java/com/mcfly/springtemp/io/ByteArrayInputOutputStreams.java
+++ src/main/java/com/mcfly/springtemp/io/ByteArrayInputOutputStreams.java
@@ -2,33 +2,19 @@ package com.mcfly.springtemp.io;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 @Slf4j
 public class ByteArrayInputOutputStreams {
 
-    private static final String FILE_NAME = "che";
-
-    public static void main(String... args) throws IOException {
-        final File file = createTempFile();
-        writeBytesToFile(file);
+    public static void main(String... args) {
+        writeBytesToFile();
     }
 
-    private static File createTempFile() throws IOException {
-        final File file = File.createTempFile(FILE_NAME, null);
-        file.deleteOnExit();
-        try (PrintWriter pw = new PrintWriter(file)) {
-            IntStream.range(0, 10).forEach(i -> pw.println("Che che che"));
-            if (pw.checkError()) {
-                throw new IOException("Temp file was not created.");
-            }
-        }
-        return file;
-    }
-
-    private static void writeBytesToFile(File file) {
+    private static void writeBytesToFile() {
         byte[] bytes = new byte[10];
         try (ByteArrayOutputStream baos = new ByteArrayOutputStream(10)) {
             baos.writeBytes(new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
