30fff069a64213f3ea1f16380b03c35fa1493f9b
Andrey Polnikov <polnikov.andrey@gmail.com>
2023-08-07 01:02:56 +0300

    + StringReaderWriter

--- src/main/java/com/mcfly/springtemp/io/StringReaderWriter.java
+++ src/main/java/com/mcfly/springtemp/io/StringReaderWriter.java
@@ -2,9 +2,7 @@ package com.mcfly.springtemp.io;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.IOException;
-import java.io.StringReader;
-import java.io.StringWriter;
+import java.io.*;
 
 @Slf4j
 public class StringReaderWriter {
@@ -16,19 +14,22 @@ public class StringReaderWriter {
     private static void writeAndReadString() {
         final String str = "abcdefghij";
         String swString = "";
-        try (StringWriter sw = new StringWriter(10)) {
-            sw.write(str);
-            sw.write(str);
-            sw.write(str);
+        try (StringWriter sw = new StringWriter(10);
+             BufferedWriter bw = new BufferedWriter(sw)) {
+            bw.write(str);
+            bw.write(str);
+            bw.write(str);
+            bw.flush();
             log.info("Put string to StringWriter: {}", sw);
             swString = sw.toString();
         } catch (IOException e) {
             log.error("Exception: ", e);
         }
-        try (StringReader sr = new StringReader(swString)) {
+        try (StringReader sr = new StringReader(swString);
+             BufferedReader br = new BufferedReader(sr)) {
             final StringBuilder sb = new StringBuilder();
             final char[] buffer = new char[10];
-            while (sr.read(buffer) > -1) {
+            while (br.read(buffer) > -1) {
                 sb.append(buffer);
             }
             log.info("Read string from StringReader: {}", sb);
